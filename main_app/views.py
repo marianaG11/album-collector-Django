331eda2824efce4ce12d52f6e910c6bed8d7dd87
marianaG11 <mariana.garcia.16@icloud.com>
2022-04-22 16:41:13 -0500

    include static files

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+# Add the following import
+from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+  return HttpResponse('<h1>Hello</h1>')
+
+def about(request):
+    return render(request, 'about.html')
