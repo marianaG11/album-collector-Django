23e81d50062a7bc47f8428ab3d49af90fdeb4003
marianaG11 <mariana.garcia.16@icloud.com>
2022-04-26 18:25:39 -0500

    add a Song Model

--- main_app/models.py
+++ main_app/models.py
@@ -15,3 +15,15 @@ class Album(models.Model):
 
     def get_absolute_url(self):
         return reverse('detail', kwargs={'album_id': self.id})
+
+
+class Song(models.Model):
+    name = models.CharField(max_length=100)
+    minutes = models.CharField(max_length=100)
+
+    #create an album_id FK
+    #FK field type is used to create a 1:M relationship
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    
+    # def __str__(self):
+    #     return f"{self.get_minutes_display()} on {self.name}"
